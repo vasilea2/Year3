f2941e113d1c9346155b93ab99908bef5f3372c9
andreivasile1010 <andreivasile1010@gmail.com>
2018-10-11 07:51:36 -0400

    almost done with week2

--- 340CT/TEACHING-MATERIALS-master-myVersion/exercises/01_nodejs/02_dynamic_website/bookshop/index.js
+++ 340CT/TEACHING-MATERIALS-master-myVersion/exercises/01_nodejs/02_dynamic_website/bookshop/index.js
@@ -8,6 +8,7 @@ const handlebars = require('express-handlebars').create({defaultLayout: 'main'})
 const bodyParser = require('body-parser')
 const app = express()
 app.use(express.static('public'))
+app.use(express.static('css'))
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.engine('handlebars', handlebars.engine)
@@ -22,15 +23,15 @@ const db = new sqlite3.Database('./bookshop.db', (err) => {
 	console.log('Connected to the "bookshop.db" SQlite database.')
 })
 
-// app.get('/', async(req, res) => {
-// 	const sql = 'SELECT * FROM books;'
-// 	console.log(sql)
-// 	db.all(sql, (err, data) => {
-// 		if(err) console.error(err.message)
-// 		console.log(JSON.stringify(data, null, 2))
-// 		res.render('home', {books: data})
-// 	})
-// })
+app.get('/oldindex', async(req, res) => {
+	const sql = 'SELECT * FROM books;'
+	console.log(sql)
+	db.all(sql, (err, data) => {
+		if(err) console.error(err.message)
+		console.log(JSON.stringify(data, null, 2))
+		res.render('home', {books: data})
+	})
+})
 
 app.get('/', async(req, res) => {
 	let sql = 'SELECT id, title, description FROM books;'
@@ -42,9 +43,19 @@ app.get('/', async(req, res) => {
 			WHERE upper(title) LIKE upper("%${req.query.q}%") 
 			OR upper(description) LIKE upper("%${req.query.q}%")
 			OR upper(author) LIKE upper("%${req.query.q}%")
-			OR upper(publisher) LIKE upper("%${req.query.q}%");`
-		q = req.query.q
+			OR upper(publisher) LIKE upper("%${req.query.q}%")`
+//       OR year = CAST("${req.query.q}" AS INTEGER);`
+// 		q = ${req.query.q}
 	}
+  
+  let queryInt = parseInt(req.query.q, 10) 
+  if (isNaN(queryInt)) {
+    console.log("Not an int");
+    sql += ";"
+  } else {
+    sql += ` OR publication_year = ${queryInt};` 
+  }
+    
 	// --------
 	db.all(sql, (err, data) => {
 		if(err) console.error(err.message)
@@ -53,26 +64,21 @@ app.get('/', async(req, res) => {
 			list += `<li>${book.id}:${book.title}</br>${book.description}</br></li>`
 		}
 		if (req.query.q !== undefined) {
-			resultString = data.length + " results for searching '" + q + "'"
-			data.forEach(function (value, i) {
-				data[i].query = q
-			})
-			// for (const book of data) 
-			// data[0].query = "node"
-			console.log(data[2].query)
+      resultString = data.length + " results for searching '" + req.query.q + "'"
 		}
 		list += '</ol>'
 		// --------
-		res.render('newindex', {books: data, result: resultString, locals: {query: q}})
+		res.render('newindex', {books: data, result: resultString, query: q})
 		// --------
 	})
 })
 
-app.get('/details/:query/:id', (req, res) => {
+app.get('/details/:id', (req, res) => {
 	console.log(req.params.id)
 	const sql = `SELECT * FROM books WHERE id = ${req.params.id};`
 	console.log(sql)
-	let q = req.params.query
+	const q = req.query.q
+  console.log(q)
 	db.get(sql, (err, data) => {
 		if(err) console.error(err.message)
 		data.description = data.description.replace(/\n/g, "</p><p>")
@@ -84,8 +90,8 @@ app.get('/form', async(req, res) => res.render('form'))
 
 app.post('/add', async(req, res) => {
 	console.log(req.body)
-	const sql = `INSERT INTO books(title, isbn, description)
-				VALUES("${req.body.title}", "${req.body.isbn}", "${req.body.description}");`
+	const sql = `INSERT INTO books(title, isbn, description, author, publisher, publication_year)
+				VALUES("${req.body.title}", ${req.body.isbn}, "${req.body.description}", "${req.body.author}", "${req.body.publisher}", ${req.body.year});`
 	console.log(sql)
 	db.run(sql, err => {
 		if(err) console.error(err.message)
