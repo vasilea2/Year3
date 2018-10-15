977d1942cf24d9ff5f3f4c5ad4545218fe8158a9
Andrei Vasile <vasilea2@coventry.ac.uk>
2018-10-15 22:10:56 +0100

    TDD done

--- 340CT/TEACHING-MATERIALS-master-myVersion/exercises/02_tdd/express/todo/__tests__/todo.test.js
+++ 340CT/TEACHING-MATERIALS-master-myVersion/exercises/02_tdd/express/todo/__tests__/todo.test.js
@@ -25,6 +25,43 @@ describe('add', () => {
 		expect(items.length).toBe(1)
 	})
 
+	test('adding an empty item string', () => {
+		expect.assertions(1)
+		try {
+			todo.add('bread', 1)
+			todo.add('', '')
+		} catch(err) {
+			expect(err.message).toBe('item is blank string')
+		}
+	})
+
+	test('adding a non-integer quantity', () => {
+		expect.assertions(1)
+		try {
+			const myIndex = 1
+			todo.add('bread', 1)
+			todo.add('flour', 'one')
+			const items = todo.getAll()
+			console.log(items[myIndex].qty)
+			expect(items[myIndex].qty).toEqual(jasmine.any(Number))
+		} catch(err) {
+			expect(err.message).toBe('quantity is non-integer')
+		}
+	})
+
+	test('adding an empty quantity', () => {
+		expect.assertions(1)
+		try {
+			const myIndex = 1
+			todo.add('bread', 1)
+			todo.add('flour', '')
+			const items = todo.getAll()
+			expect(items[myIndex].qty).toEqual(0)
+		} catch(err) {
+			expect(err.message).toBe('quantity is empty')
+		}	
+	})
+
 })
 
 describe('clear', () => {
@@ -74,4 +111,100 @@ describe('getAll', () => {
 		}
 	})
 
+	test('retrieving list with duplicate items', () => {
+		expect.assertions(2)
+		try {
+			const myQty = 1
+			const myIndex = 0
+			todo.add('bread', myQty)
+			todo.add('bread', myQty)
+			const items = todo.getAll()
+			expect(items.length).toBe(1)
+			expect(items[myIndex].qty).toBe(2*myQty)
+			todo.clear()
+		} catch(err) {
+			expect(err.message).toBe('list has duplicate item names')
+		}
+	})
+
+	test ('retrieving list with low level duplicates (same item, different case)', () => {
+		expect.assertions(1)
+		try {
+			todo.add('bread', 7)
+			todo.add('bREad', 3)
+			const items = todo.getAll()
+			expect(items.length).toBe(1)
+			todo.clear()
+		} catch(err) {
+			expect(err.message).toBe('item found twice using different case')
+		}
+	})
+
+})
+
+describe('delete', () => {
+
+	beforeEach( () => todo.add('bread', 1))
+	afterEach( () => todo.clear())
+
+	test ('delete one item from the list', () => {
+		expect.assertions(1)
+		try {
+			const myIndex = 1
+			todo.add('cookie', 2)
+			const items = todo.getAll()
+			todo.delete(myIndex)
+			expect(items.length).toBe(1)
+		} catch(err) {
+			expect(err.message).toBe('failed to delete one item')
+		}
+	})
+
+	test ('delete an inexistent key from the list', () => {
+		try {
+			const wrongIndex = 3
+			todo.add('cookie', 2)
+			const items = todo.getAll()
+			todo.delete(wrongIndex)
+			expect(items.length).toBe(2)
+		} catch(err) {
+			expect(err.message).toBe('deleting an inexistent index')
+		}
+	})
+
+	test ('input is not an integer (is string)', () => {
+		try {
+			const wrongIndex = "asd"
+			todo.add('cookie', 2)
+			const items = todo.getAll()
+			todo.delete(wrongIndex)
+			expect(items.length).toBe(2)
+		} catch(err) {
+			expect(err.message).toBe('deleting using a string key')
+		}
+	})
+
+	test ('input is not an integer (is float)', () => {
+		try {
+			const wrongIndex = 0.7
+			todo.add('cookie', 2)
+			const items = todo.getAll()
+			todo.delete(wrongIndex)
+			expect(items.length).toBe(2)
+		} catch(err) {
+			expect(err.message).toBe('deleting using a float key')
+		}
+	})
+
+	test ('input is negative number', () => {
+		try {
+			const wrongIndex = -0.2
+			todo.add('cookie', 2)
+			const items = todo.getAll()
+			todo.delete(wrongIndex)
+			expect(items.length).toBe(2)
+		} catch(err) {
+			expect(err.message).toBe('deleting using a negative key')
+		}
+	})
 })
--- 340CT/TEACHING-MATERIALS-master-myVersion/exercises/02_tdd/express/todo/modules/todo.js
+++ 340CT/TEACHING-MATERIALS-master-myVersion/exercises/02_tdd/express/todo/modules/todo.js
@@ -9,16 +9,41 @@ module.exports.clear = () => {
 }
 
 module.exports.add = (item, qty) => {
-	data.push({item: item, qty: qty})
+	if (item.length === 0) throw new Error('item is blank string')
+	if (isNaN(qty)) throw new Error('quantity is non-integer')
+	if (qty === '') {
+		qty = 0
+		console.log('quantity is empty')
+	}
+	data.push({item: item.toLowerCase(), qty: qty})
 }
 
 module.exports.getAll = () => {
 	if(data.length === 0) throw new Error('empty list')
 	for(const key in data) data[key].key = key
+
+	// Remove duplicate item names, adding up the quantity to the lower key one
+	let sameItems = []
+	for (const index in data) {
+		let tmpItem = data[index].item
+		if (sameItems.length === 0) sameItems.push(tmpItem)
+		else if (sameItems.includes(tmpItem) === false) sameItems.push(tmpItem)
+		else for (let j = 0; j < index; j++) if (data[j].item === tmpItem) {
+			data[j].qty = parseInt(data[j].qty, 10)
+			data[index].qty = parseInt(data[index].qty, 10)			
+			data[j].qty += data[index].qty
+			data.splice(-1, 1)
+		}
+	}	
 	return data
 }
 
 module.exports.delete = key => {
-	console.log(`delete key ${key}`)
+	data.splice(key, 1)
+	if (isNaN(key)) throw new Error('deleting using a string key')
+	if (key < 0) throw new Error('deleting using a negative key')
+	const floatCheck = (key % 1 === 0)
+	if (floatCheck === false) throw new Error('deleting using a float key')
+	if (key > data.length) throw new Error('deleting an inexistent index')
 	return
 }
